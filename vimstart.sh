685bc44c7c9bb636a32c4788f38e9a321267c16e
username77177 <username77177@gmail.com>
2020-02-05 13:32:01 +0300

    05/02/2020 13:32:00

---
+++ vimstart.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+git clone https://www.github.com/username77177/vimstart.git $HOME/pushvim
+cat $HOME/pushvim/hellomsg
+
+echo "Would you like to install vim, npm, python3, prettier and fzf to your system? 1 - yeah, 0 - no"
+read inp
+if [[ inp == "1"]]
+then
+	#Installation Part
+	sudo apt install npm python3 vim
+	sudo pip3 install fzf
+	sudo npm install prettier
+fi
+
+mv $HOME/.vimrc $HOME/.vimrc.bckp
+mv $HOME/pushvim/vimrc $HOME/.vimrc
+
