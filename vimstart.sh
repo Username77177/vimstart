36188c5506eec45cc5cc34b3f6d98bbb20505858
username77177 <username77177@gmail.com>
2020-02-05 13:53:22 +0300

    05/02/2020 13:53:18 | final changes

--- vimstart.sh
+++ vimstart.sh
@@ -4,7 +4,7 @@ cat $HOME/pushvim/hellomsg
 
 echo "Would you like to install vim, npm, python3, prettier and fzf to your system? 1 - yeah, 0 - no"
 read inp
-if [[ inp == "1" ]]
+if [[ $inp == "1" ]]
 then
 	#Installation Part
 	sudo apt install npm python3 vim
