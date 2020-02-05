2945d70de2e643a4b169366a1281c3826ec9a6b8
username77177 <username77177@gmail.com>
2020-02-05 13:55:48 +0300

    05/02/2020 13:55:47

--- vimstart.sh
+++ vimstart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git clone https://www.github.com/username77177/vimstart.git $HOME/pushvim
+git clone https://github.com/Username77177/vimstart.git $HOME/pushvim
 cat $HOME/pushvim/hellomsg
 
 echo "Would you like to install vim, npm, python3, prettier and fzf to your system? 1 - yeah, 0 - no"
